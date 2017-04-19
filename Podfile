f754973a0f113680485ec849c9fe790c723776a1
　buyiyang <776949529@qq.com>
2017-04-19 21:52:26 +0800

    上传项目文件

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+target 'iOSStar' do
+    # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+    use_frameworks!
+    
+    # Pods for iosblackcwpard
+    
+    pod 'Alamofire', '~> 4.2.0'
+    pod 'SVProgressHUD', '~> 2.0.3'
+    pod 'Kingfisher', '~> 3.2.2'
+    pod 'Qiniu', '~> 7.1'
+    pod 'Fabric'
+    pod 'Crashlytics'
+    pod 'SnapKit'
+    pod 'RealmSwift'
+end
+
---
+++ iOSStar/Other/BaseTabBarController.swift
@@ -0,0 +1,85 @@
+//
+//  BaseTabBarController.swift
+//  iosblackcard
+//
+//  Created by J-bb on 17/4/14.
+//  Copyright © 2017年 YunDian. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController ,UITabBarControllerDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setViewControllers()
+        
+    }
+
+    func initcustomer(){
+    
+        let storyboardNames = ["Home","Deal","User"]
+        let titles = ["首页","交易","个人中心"]
+        for (index, name) in storyboardNames.enumerated() {
+            let storyboard = UIStoryboard.init(name: name, bundle: nil)
+            let controller = storyboard.instantiateInitialViewController()
+            controller?.tabBarItem.title = titles[index]
+            controller?.tabBarItem.image = UIImage.init(named: "\(storyboardNames[index])UnSelect")?.withRenderingMode(.alwaysOriginal)
+            controller?.tabBarItem.selectedImage = UIImage.init(named: "\(storyboardNames[index])Select")?.withRenderingMode(.alwaysOriginal)
+//            controller?.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(rgbHex: 0x666666)], for: .normal)
+//            controller?.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: AppConst.Color.CMain], for: .selected)
+            addChildViewController(controller!)
+            
+            delegate = self
+            
+            
+            
+        }
+        
+
+    }
+    
+    func setViewControllers()  {
+
+        let vcNameArray = ["ViewController",
+                           "ViewController",
+//                           "ClubViewController",
+//                           "PrivateAdvisoryViewController",
+                           "ViewController",
+                           "ViewController"]
+        let titlesArray = ["黑卡",
+                           "部落",
+//                           "俱乐部",
+//                           "私董会",
+                           "管家",
+                           "我的"]
+        var vcArray = [UIViewController]()
+        let namespace = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String;
+        for index in 0...vcNameArray.count - 1 {
+            let name = namespace + "." + vcNameArray[index]
+            let cls = NSClassFromString(name) as? UIViewController.Type
+            guard cls != nil else {
+                return
+            }
+            let vc = cls!.init()
+            let item = UITabBarItem(title: titlesArray[index], image: nil, tag: 1000+index)
+            if vcNameArray[index] == "BlackCardViewController" {
+                vc.tabBarItem = item
+                vcArray.append(vc)
+            } else {
+                let nc = BaseNavigationController(rootViewController: vc)
+                nc.tabBarItem = item
+                vcArray.append(nc)
+            }
+        }
+        viewControllers = vcArray
+    }
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+//        let registVC = LoginVC()
+//        
+//        let vc =  BaseNavigationController.init(rootViewController: registVC)
+//        present(vc, animated: true, completion: nil)
+    }
+}
