b372d48719f499e5a2f75367980eba1901206b10
BBRick <jiaobiao0505@163.com>
2017-04-26 16:08:45 +0800

    AddedContactList

--- iOSStar/Scenes/News/Controller/NewsDetailViewController.swift
+++ iOSStar/Scenes/News/Controller/NewsDetailViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
-
+import WebKit
 class NewsDetailViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "资讯详情"
+
+        let webView = WKWebView(frame: view.bounds)
+        
+        
+        let url = URL(fileURLWithPath: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493098902992&di=74b063fe6c4fe1b887fe976d4c219bd3&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F13%2F61%2F00%2F61a58PICtPr_1024.jpg")
+        let request = URLRequest(url: url)
+        webView.load(request)
+        view.addSubview(webView)
+        
     }
     
 
