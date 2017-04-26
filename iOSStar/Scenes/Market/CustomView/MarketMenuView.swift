298acecaaeb253398a15bcb2b8c5717ee1d41f61
BBRick <jiaobiao0505@163.com>
2017-04-26 17:13:55 +0800

    merge

---
+++ iOSStar/Scenes/Market/CustomView/MarketMenuView.swift
@@ -0,0 +1,29 @@
+//
+//  MarketMenuView.swift
+//  iOSStar
+//
+//  Created by J-bb on 17/4/26.
+//  Copyright © 2017年 YunDian. All rights reserved.
+//
+
+import UIKit
+
+class MarketMenuView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
