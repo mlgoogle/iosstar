f2d8aa46352b5850230a4a0e8db77b727bc896b1
　buyiyang <776949529@qq.com>
2017-06-21 20:45:25 +0800

    修改设置支付密码

--- iOSStar/General/Helper/AppConfigHelper.swift
+++ iOSStar/General/Helper/AppConfigHelper.swift
@@ -4,7 +4,6 @@
 //
 //  Created by J-bb on 17/5/26.
 //  Copyright © 2017年 YunDian. All rights reserved.
-//
 
 import UIKit
 import UserNotifications
@@ -17,7 +16,7 @@ let kGtAppSecret:String = "4DXXxrRirbAhqYJor3THd"
 
 class AppConfigHelper: NSObject {
     
-    var dealResult:[String : String] = ["-1" : "订单取消", "0" : "扣费成功", "1" : "转让方持有时间不足", "2" : "求购方金币不足"]
+    var dealResult:[String : String] = ["-1" : "订单取消", "0" : "扣费成功", "-2" : "转让方持有时间不足", "-3" : "求购方金币不足"]
     var updateModel:UpdateParam?
     lazy var alertView: TradingAlertView = {
         let alertView = Bundle.main.loadNibNamed("TradingAlertView", owner: nil, options: nil)?.first as! TradingAlertView
