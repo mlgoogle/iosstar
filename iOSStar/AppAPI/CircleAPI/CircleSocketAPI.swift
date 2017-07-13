963d736095ba784bf522d52ef1fda22e934d91d8
BBRick <jiaobiao0505@163.com>
2017-07-13 20:22:46 +0800

    添加嵌套回调

--- iOSStar/AppAPI/CircleAPI/CircleSocketAPI.swift
+++ iOSStar/AppAPI/CircleAPI/CircleSocketAPI.swift
@@ -12,7 +12,7 @@ class CircleSocketAPI: BaseSocketAPI, CircleAPI{
     //朋友圈
     func requestCircleList(requestModel:CircleListRequestModel,complete: CompleteBlock?, error: ErrorBlock?) {
         let packet = SocketDataPacket(opcode: .circleList, model: requestModel)
-        startModelsRequest(packet, listName: "circle_list", modelClass: CircleListModel.self, complete: complete, error: error)
+        startModelsRequest(packet, listKey: "circle_list", modelClass: CircleListModel.self, complete: complete, error: error)
     }
     //评论动态
     func commentCircle(requestModel:CommentCircleModel,complete: CompleteBlock?, error: ErrorBlock?) {
