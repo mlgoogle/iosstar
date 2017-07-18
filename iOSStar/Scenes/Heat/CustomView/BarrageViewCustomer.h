4fbf1592537cc5f03dddc8189116e8392b179cdc
BBRick <jiaobiao0505@163.com>
2017-07-18 12:02:46 +0800

    bugfixes

---
+++ iOSStar/Scenes/Heat/CustomView/BarrageViewCustomer.h
@@ -0,0 +1,17 @@
+//
+//  BarrageViewCustomer.h
+//  iOSStar
+//
+//  Created by sum on 2017/7/17.
+//  Copyright © 2017年 YunDian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BarrageViewCustomer : UIView
+
+@property(nonatomic, strong) UIImageView *imageView;
+@property(nonatomic, strong) UILabel *titleLabel;
+@property(nonatomic, strong) UIView *backView;
+@end
---
+++ iOSStar/Scenes/Heat/CustomView/YD_Barrage.swift
@@ -0,0 +1,17 @@
+//
+//  YD_Barrage.swift
+//  iOSStar
+//
+//  Created by sum on 2017/7/17.
+//  Copyright © 2017年 YunDian. All rights reserved.
+//
+
+import UIKit
+import BarrageRenderer
+class YD_Barrage: BarrageWalkSprite {
+    override init() {
+        super.init()
+        trackNumber = 5
+        setValue("UILabel", forKey: "viewClassName")
+    }
+}
