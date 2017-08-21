0b1ef73049297e83ef77689bab1fa484b24e9ed3
　buyiyang <776949529@qq.com>
2017-08-21 10:41:14 +0800

    播放网页的url

---
+++ iOSStar/Scenes/Discover/Controllers/PlayNetVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlayNetVC.swift
+//  iOSStar
+//
+//  Created by sum on 2017/8/21.
+//  Copyright © 2017年 YunDian. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+class PlayNetVC: UIViewController {
+
+    var player : PLPlayer?
+    var playUrl : String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let option = PLPlayerOption.default()
+        player = PLPlayer.init(url: NSURL.fileURL(withPath: playUrl), option: option)
+        self.view.addSubview((player?.playerView)!)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
